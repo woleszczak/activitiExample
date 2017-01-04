3ed1cd167adf044f69215ea4e5e570d713936bfc
Wojciech Oleszczak <wojciech.oleszczak@comarch.com>
2017-01-04 11:05:47 +0100

    Implementation

--- activitiExample/src/main/java/com/woleszczak/activitiExample/App.java
+++ activitiExample/src/main/java/com/woleszczak/activitiExample/App.java
@@ -1,13 +1,59 @@
 package com.woleszczak.activitiExample;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.identity.Group;
+import org.activiti.engine.identity.User;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	/*@Bean
+	CommandLineRunner init(final RepositoryService repositoryService, final RuntimeService runtimeService,
+			final TaskService taskService) {
+
+		return new CommandLineRunner() {
+
+			public void run(String... strings) throws Exception {
+				Map<String, Object> variables = new HashMap<String, Object>();
+				variables.put("applicantName", "John Doe");
+				variables.put("email", "john.doe@activiti.com");
+				variables.put("phoneNumber", "123456789");
+				runtimeService.startProcessInstanceByKey("hireProcess", variables);
+			}
+		};
+	}*/
+	
+	@Bean
+	InitializingBean usersAndGroupsInitializer(final IdentityService identityService) {
+
+	    return new InitializingBean() {
+	        public void afterPropertiesSet() throws Exception {
+
+	            Group group = identityService.newGroup("user");
+	            group.setName("users");
+	            group.setType("security-role");
+	            identityService.saveGroup(group);
+
+	            User admin = identityService.newUser("admin");
+	            admin.setPassword("admin");
+	            identityService.saveUser(admin);
+
+	        }
+	    };
+	}
 }
---
+++ activitiExample/src/main/java/com/woleszczak/activitiExample/ResumeService.java
@@ -0,0 +1,12 @@
+package com.woleszczak.activitiExample;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResumeService {
+
+	public void storeResume() {
+        System.out.println("Storing resume ...");
+    }
+	
+}
